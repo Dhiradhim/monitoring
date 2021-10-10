aef00091d1636d83c9b22e1b3fa3cfc5ad8490af
Dhiradhim <dhiradhim@gmail.com>
2021-10-10 10:21:46 +0800

    delete some files

--- dn/year.php
+++
@@ -1,4 +0,0 @@
-<?php
-date_default_timezone_set('UTC+7');?>
-<?php $number=3;?>
-DN.<?=date('Y')?>-<?=date('m')?>/HUB-SCM/<?=str_pad($number, 4, '0', STR_PAD_LEFT)?>
--- view/koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","adminrdc123");
-mysqli_select_db ($con, "inventorynoc");
-?>
